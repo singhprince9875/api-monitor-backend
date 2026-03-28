5df4020048a305711d0ab3695bcc55b94e37bd4e
singhprince9875 <princekumarsingh9875@gmail.com>
2026-03-28 11:05:16 +0530

    till here it working

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const pool = require("./db");
 const express = require("express");
 const axios = require("axios");
-const cors = require("cors");
+const cors = require("cors");   
 
 const app = express();
 app.use(cors());
